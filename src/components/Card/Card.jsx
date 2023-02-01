1d9d95159ab02d2ccb94bcbdf6b933a754e1f509
ShehzadQadri <63380192+ShehzadQadri@users.noreply.github.com>
2023-02-01 11:09:40 +0500

    update changes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { AnimateSharedLayout } from 'framer-motion'
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import './Card.css'
 
 
 const Card = (props) => {
@@ -8,7 +11,7 @@ const Card = (props) => {
     return (
         <AnimateSharedLayout>
             {expanded ?
-                // <ExpandedCard /> :
+                // <ExpandededCard /> :
                 ('Expanded') :
                 < CompactCard param={props} set />}
         </AnimateSharedLayout>
@@ -21,9 +24,18 @@ const Card = (props) => {
 function CompactCard({ param }) {
     const Png = param.png
     return (
-        <div className="CompactCard">
+        <div className="CompactCard"
+            style={{
+                background: param.color.backGround,
+                boxShadow: param.color.boxShadow,
+            }}
+        >
             <div className="radialBar">
-                Chart
+                <CircularProgressbar
+                    value={param.barValue}
+                    text={`${param.barValue}%`}
+
+                />
             </div>
             <div className="detail">
                 <Png />
